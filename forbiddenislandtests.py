ecab8c9319a911f328b27a7e0753da3a0454a02b
Alex Bilson <acbilson@gmail.com>
2016-01-18 11:07:22 -0600

    First commit to add basic test project and board design.

---
+++ forbiddenislandtests.py
@@ -0,0 +1,11 @@
+import unittest
+
+class Test(unittest.TestCase):
+
+  """ Testing the something """
+
+  # def setUp(self):
+    # self.tio = FakeIO()
+
+  # def tearDown(self):
+    # self.tio = None
