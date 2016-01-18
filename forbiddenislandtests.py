69fe44f7f794e7b79f171cded13c9264f87a58eb
Alex Bilson <acbilson@gmail.com>
2016-01-18 16:35:12 -0600

    Able to create and modify the island board

--- forbiddenislandtests.py
+++ forbiddenislandtests.py
@@ -1,36 +1,140 @@
+import sys
+sys.path.append("C:\\SourceCode\\PersonalRepo\\Python\\PyLexLib")
+from iofactory import *
+
 import unittest
 from island import *
 
 class TestIsland(unittest.TestCase):
 
   """ Testing the Island class """
+  island = None
 
-  def setup(self):
+  def setUp(self):
     self.island = Island()
 
   def test_island_ctor(self):
     island = Island()
-      
-class TestTile(unittest.TestCase):
 
+  def test_island_getTile(self):
+    self.island.board = TestConstants.GetTileBoard
+
+    actual = self.island.getTile(TileName.CliffsOfAbandon)
+
+    expected = Tile(name=TileName.CliffsOfAbandon, 
+                    player=PlayerType.Engineer, 
+                    status=TileStatus.Sunken)
+
+    self.assertEqual(expected, actual)
+
+  def test_island_updateTile(self):
+    self.island.board = TestConstants.GetTileBoard
+    tile = Tile(name=TileName.CliffsOfAbandon, 
+                player=PlayerType.Engineer, 
+                status=TileStatus.Raised)
+
+    self.island.updateTile(tile)
+
+    expected = TestConstants.UpdateTileBoard
+    self.assertEqual(expected, self.island.board)
+
+
+class TestTile(unittest.TestCase):
   """ Testing the Tile class """
   tile = None
 
   def setUp(self):
-    self.tile = Tile("COA")
+    self.tile = Tile()
 
   def test_tile_ctor(self):
-    tile = Tile("COA")
+    tile = Tile()
 
     self.assertIsNotNone(tile.name)
 
   def test_tile_get(self):
-    tile = Tile("COA")
+    tile = Tile()
 
     actual = tile.get()
 
-    expected = ('/COA\\' +
+    expected = ('/   \\' +
                 '|   |'  +
                 '\   /')
 
     self.assertEqual(expected, actual)
+
+class TestConstants():
+
+  EmptyBoard = (
+  '             /   \ /   \\              \n' + 
+  '             |   | |   |              \n' +
+  '             \   / \   /              \n' +
+  '                                      \n' + 
+  '      /   \ /   \ /   \ /   \\         \n' + 
+  '      |   | |   | |   | |   |         \n' + 
+  '      \   / \   / \   / \   /         \n' +
+  '                                      \n' + 
+  ' /   \ /   \ /   \ /   \ /   \ /   \\  \n' +
+  ' |   | |   | |   | |   | |   | |   |  \n' +
+  ' \   / \   / \   / \   / \   / \   /  \n' +
+  '                                      \n' + 
+  ' /   \ /   \ /   \ /   \ /   \ /   \\  \n' +
+  ' |   | |   | |   | |   | |   | |   |  \n' +
+  ' \   / \   / \   / \   / \   / \   /  \n' +
+  '                                      \n' + 
+  '       /   \ /   \ /   \ /   \\        \n' +
+  '       |   | |   | |   | |   |        \n' +
+  '       \   / \   / \   / \   /        \n' +
+  '                                      \n' + 
+  '             /   \ /   \\              \n' +
+  '             |   | |   |              \n' +
+  '             \   / \   /              \n')
+
+  GetTileBoard = (
+  '             /   \ /   \\              \n' + 
+  '             |   | |   |              \n' +
+  '             \   / \   /              \n' +
+  '                                      \n' + 
+  '      /   \ /   \ /   \ /   \\         \n' + 
+  '      |   | |   | |   | |   |         \n' + 
+  '      \   / \   / \   / \   /         \n' +
+  '                                      \n' + 
+  ' /   \ /   \ /   \ /   \ /   \ /   \\  \n' +
+  ' |   | |   | |   | |   | |   | |   |  \n' +
+  ' \   / \   / \   / \   / \   / \   /  \n' +
+  '                                      \n' + 
+  ' /   \ /   \ /   \ /   \ /COA\ /   \\  \n' +
+  ' |   | |   | |   | |   | |ENG| |   |  \n' +
+  ' \   / \   / \   / \   / \SNK/ \   /  \n' +
+  '                                      \n' + 
+  '       /   \ /   \ /   \ /   \\        \n' +
+  '       |   | |   | |   | |   |        \n' +
+  '       \   / \   / \   / \   /        \n' +
+  '                                      \n' + 
+  '             /   \ /   \\              \n' +
+  '             |   | |   |              \n' +
+  '             \   / \   /              \n')
+
+  UpdateTileBoard = (
+  '             /   \ /   \\              \n' + 
+  '             |   | |   |              \n' +
+  '             \   / \   /              \n' +
+  '                                      \n' + 
+  '      /   \ /   \ /   \ /   \\         \n' + 
+  '      |   | |   | |   | |   |         \n' + 
+  '      \   / \   / \   / \   /         \n' +
+  '                                      \n' + 
+  ' /   \ /   \ /   \ /   \ /   \ /   \\  \n' +
+  ' |   | |   | |   | |   | |   | |   |  \n' +
+  ' \   / \   / \   / \   / \   / \   /  \n' +
+  '                                      \n' + 
+  ' /   \ /   \ /   \ /   \ /COA\ /   \\  \n' +
+  ' |   | |   | |   | |   | |ENG| |   |  \n' +
+  ' \   / \   / \   / \   / \   / \   /  \n' +
+  '                                      \n' + 
+  '       /   \ /   \ /   \ /   \\        \n' +
+  '       |   | |   | |   | |   |        \n' +
+  '       \   / \   / \   / \   /        \n' +
+  '                                      \n' + 
+  '             /   \ /   \\              \n' +
+  '             |   | |   |              \n' +
+  '             \   / \   /              \n')
--- island.py
+++ island.py
@@ -1,31 +1,130 @@
 class Island(object):
 
+  NameWidth = 3
+  BoardWidth = 39
+
   def __init__(self):
-    self.name = "Test"
+    self.board = Constants.EmptyBoard
+
+  def getTile(self, tileName):
+    i = self._findTileNameIndex(tileName)
+    top = self._getTileName(i)
+    mid = self._getTileUser(i)
+    bot = self._getTileStatus(i)
+    return Tile(name=top, player=mid, status=bot)
+
+  def updateTile(self, tile):
+    """ Currently gets substrings for all non-tile pieces, then adds them back in the appropriate place.  Highly
+    resource intensive, but no other way so far """
+    ni = self._findTileNameIndex(tile.name)
+    pi = self._findTilePlayerIndex(ni)
+    # print("pi is: ", pi, "actual is: ", self.board.index('ENG'))
+    si = self._findTileStatusIndex(ni)
+    # print("si is: ", si, "actual is: ", self.board.index('SNK'))
+
+    leftTop = self.board[0:ni-1]
+    rightTop = self.board[ni+self.NameWidth+1:pi-1]
+    rightMid = self.board[pi+self.NameWidth+1:si-1]
+    rightBot = self.board[si+self.NameWidth+1:len(self.board)]
+
+    self.board = (leftTop + tile.getTop() + rightTop + tile.getMid() + rightMid + tile.getBot() + rightBot)
+    # print(leftTop + '/COA\\' + rightTop + '|ENG|' + rightMid + '\   /' + rightBot)
+
+  def _findTileNameIndex(self, tileName):
+    return self.board.index(tileName)
+
+  def _findTilePlayerIndex(self, nameIndex):
+    return nameIndex + self.BoardWidth
+
+  def _findTileStatusIndex(self, nameIndex):
+    return nameIndex + (self.BoardWidth*2)
+
+  def _getTileName(self, index):
+    start = index
+    end = index + self.NameWidth
+    return self.board[start:end]
+
+  def _getTileUser(self, index):
+    start = index + self.BoardWidth
+    end = (index + self.NameWidth) + self.BoardWidth
+    return self.board[start:end]
+
+  def _getTileStatus(self, index):
+    start = index + (self.BoardWidth * 2)
+    end = (index + self.NameWidth) + (self.BoardWidth * 2)
+    return self.board[start:end]
+
+  def get(self):
+    return self.board
 
 class TileName():
   CliffsOfAbandon = "COA"
+
+class PlayerType():
+  Engineer = "ENG"
+
+class TileStatus():
+  Sunken = "SNK"
+  Raised = "   "
 
 class Tile(object):
 
   Empty = '   '
 
-  def __init__(self, name):
+  def __init__(self, name=Empty, player=Empty, status=Empty):
     self.name = name
-    self.player = self.Empty
-    self.status = self.Empty
+    self.player = player
+    self.status = status
+
+  def __eq__(self, other):
+    if isinstance(other, self.__class__):
+      return (self.name == other.name and 
+              self.player == other.player and
+              self.status == other.status)
+
+  def __ne__(self, other):
+    return not self.__eq__(other)
 
   def get(self):
-    top = self._addTop()
-    mid = self._addMid()
-    bot = self._addBot()
+    top = self.getTop()
+    mid = self.getMid()
+    bot = self.getBot()
     return (top + mid + bot)
 
-  def _addTop(self):
+  def getTop(self):
     return '/' + self.name + '\\'
 
-  def _addMid(self):
+  def getMid(self):
     return '|' + self.player + '|'
 
-  def _addBot(self):
+  def getBot(self):
     return '\\' + self.status + '/'
+
+
+
+class Constants():
+
+  EmptyBoard = (
+  '            /   \ /   \\\n' + 
+  '            |   | |   |\n' +
+  '            \   / \   /\n' +
+  '\n' + 
+  '     /   \ /   \ /   \ /   \\\n' + 
+  '     |   | |   | |   | |   |\n' + 
+  '     \   / \   / \   / \   /\n' +
+  '\n' +
+  '/   \ /   \ /   \ /   \ /   \ /   \\\n' +
+  '|   | |   | |   | |   | |   | |   |\n' +
+  '\   / \   / \   / \   / \   / \   /\n' +
+  '\n' +
+  '/   \ /   \ /   \ /   \ /   \ /   \\\n' +
+  '|   | |   | |   | |   | |   | |   |\n' +
+  '\   / \   / \   / \   / \   / \   /\n' +
+  '\n' +
+  '      /   \ /   \ /   \ /   \\\n' +
+  '      |   | |   | |   | |   |\n' +
+  '      \   / \   / \   / \   /\n' +
+  '\n' +
+  '            /   \ /   \\\n' +
+  '            |   | |   |\n' +
+  '            \   / \   /\n')
